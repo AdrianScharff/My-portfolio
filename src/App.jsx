0faf4f6da3e645cecf08a82ed72efdd6fc6dae6b
AdrianScharff <adrian18juarez@gmail.com>
2024-09-03 22:45:38 -0600

    feat: create first version of my personal portfolio

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+function App() {
+
+  return (
+    <div className="flex flex-col items-center px-6 md:px-0">
+      <h1 className="text-3xl md:text-6xl font-bold border border-sky-600 border-8 text-center p-3 md:p-6 rounded-3 mt-5">
+        Adrian Juarez - Web Developer
+      </h1>
+      <div className="my-10">
+        <div className="flex flex-col py-8 gap-6">
+          <p className="text-3xl md:text-5xl">Advanced Frontend projects</p>
+          <ul className="flex flex-col gap-2 md:ps-10">
+            <a href="https://adrianscharff.github.io/Pinterest-Projject/" className="w-fit text-sky-600 underline">Pinterest replica (CSS, no JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/ATM/" className="w-fit text-sky-600 underline">ATM (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Students-Database-Project/index.html" className="w-fit text-sky-600 underline">Student Database (JavaScript) - Only Desktop</a>
+            <a href="https://adrianscharff.github.io/Trivia-App/" className="w-fit text-sky-600 underline">Trivia App (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/React-Issues-Project/" className="w-fit text-sky-600 underline">React Issues Project (React) - Responsive</a>
+            <a href="https://moviesdatabaseadrian.netlify.app/" className="w-fit text-sky-600 underline">Movie Database (React) - Responsive</a>
+            <a href="https://full-ecommerce.netlify.app/" className="w-fit text-sky-600 underline">Ecommerce store (React) - Responsive</a>
+            <a href="https://movie-catalog-adrian.netlify.app/" className="w-fit text-sky-600 underline">Movie Catalog (React, Express, MongoDB, Node.js) - Only Desktop</a>
+          </ul>
+        </div>
+        <div className="flex flex-col py-8 gap-6">
+          <p className="text-3xl md:text-5xl">Other Frontend projects</p>
+          <ul className="flex flex-col gap-2 md:ps-10">
+            <a href="https://adrianscharf.github.io/Youtube-video-player-page/" className="w-fit text-sky-600 underline">Youtube video page (CSS, no JavaScript) - Only Desktop</a>
+            <a href="https://adrianscharff.github.io/My-portfolio-Adrian-Juarez/" className="w-fit text-sky-600 underline">First portfolio Microverse (CSS, no JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/1st-Capstone-project-MexicanFood/" className="w-fit text-sky-600 underline">Mexican Food Convention Page (CSS, no JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Awesome-Books/" className="w-fit text-sky-600 underline">Awesome Books (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Meme-Generator/" className="w-fit text-sky-600 underline">Meme Generator (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Simple-Calculator/" className="w-fit text-sky-600 underline">Simple Calculator (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Color-Flipper/" className="w-fit text-sky-600 underline">Color Flipper (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Counter/" className="w-fit text-sky-600 underline">Counter (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Dynamic-Reviews/" className="w-fit text-sky-600 underline">Dynamic Reviews (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Google-replica-htmlcss/" className="w-fit text-sky-600 underline">Google replica (CSS, no JavaScript) - Only Desktop</a>
+            <a href="https://adrianscharff.github.io/Scroll-Project/" className="w-fit text-sky-600 underline">Scroll project (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Grocery-List/" className="w-fit text-sky-600 underline">Grocery bud (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/arrays-project/" className="w-fit text-sky-600 underline">Arrays project (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/while-project/" className="w-fit text-sky-600 underline">While project (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/for-project/" className="w-fit text-sky-600 underline">For project (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/functions-project/" className="w-fit text-sky-600 underline">Functions project (JavaScript) - Only Desktop</a>
+            <a href="https://adrianscharff.github.io/Conditionals-Project/" className="w-fit text-sky-600 underline">Conditionals project (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/month-employee/" className="w-fit text-sky-600 underline">Month-employee (JavaScript) - Responsive</a>
+            <a href="https://adrianscharff.github.io/Callbacks/" className="w-fit text-sky-600 underline">Callbacks (JavaScript) - Responsive</a>
+          </ul>
+        </div>
+        <div className="flex flex-col py-8 gap-6">
+          <p className="text-3xl md:text-5xl">Backend projects</p>
+          <ul className="flex flex-col gap-2 md:ps-10">
+            <a href="https://movie-database-api-amber.vercel.app/" className="w-fit text-sky-600 underline">Simple Movie Database API (Node.js, Express)</a>
+            <a href="#" className="w-fit text-sky-600 underline">Tinder for Skills API (Node.js, Express, PostgreSQL)</a>
+            <a href="https://cinema-api-6j3q.onrender.com/" className="w-fit text-sky-600 underline">Cinema API (Node.js, Express, MongoDB)</a>
+            <a href="https://movie-catalog-api-pk5p.onrender.com/" className="w-fit text-sky-600 underline">Movie Catalog API (Node.js, Express, MongoDB)</a>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
